1db431e5e0622c8831326e699af6660d0f1537a4
Muaaz Abdus Sami <techbrothers3@gmail.com>
2024-12-25 15:25:43 +0530

    add joblisting

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "../Components/Hero";
+import Homecards from "../Components/HomeCards";
+import JobListings from "../Components/JobListings";
+import ViewAllJobs from "../Components/ViewAllJobs";
+const Homepage = () => {
+  return (
+    <>
+      <Hero />
+      <Homecards />
+      <JobListings isHome={true} />
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default Homepage;
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import JobsListings from '../Components/JobListings'
+const JobsPage = () => {
+  return <section className='bg-blue-50 px-4 py-6'>
+    <JobsListings/>
+  </section>
+  
+}
+
+export default JobsPage
